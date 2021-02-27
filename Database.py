d1d4cdf548d26893a1e2055eb026c32e2115b355
hamza <teamhh.dev@gmail.com>
2021-02-28 02:30:42 +0500

    Warning and images url fixed

--- Database.py
+++ Database.py
@@ -3,33 +3,16 @@ import mysql.connector
 
 class Database:
     def __init__(self):
-<<<<<<< HEAD
-        self.mydb = mysql.connector.connect(
+        self.appDatabase = mysql.connector.connect(
             host="localhost",
             user="root",
             password="twitter_"
         )
-        self.appCursor = self.mydb.cursor()
-=======
-        self.appDatabase = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password=""
-        )
         self.appCursor = self.appDatabase.cursor()
->>>>>>> a505de5579bccfd7715ad9f855441a8de58b635f
         self.initDatabase()
 
     def initDatabase(self):
         self.appCursor.execute("create database if not exists ecodb")
-<<<<<<< HEAD
-        self.appCursor.execute("CREATE TABLE `ecodb`.`complaints` ( `invoice_id` VARCHAR(15) NOT NULL ,  `date` DATE NOT NULL ,  `branch_address` VARCHAR(50) NOT NULL ,  `zone` VARCHAR(10) NOT NULL ,  `complaint_no` INT(10) NOT NULL ,    PRIMARY KEY  (`invoice_id`));")
-
-    def getdata(self):
-        self.appCursor.execute("SELECT * FROM hamzaalidatabase.customer")
-        for row in self.appCursor:
-            print(row)
-=======
         # self.appCursor.execute(
         #     "CREATE TABLE if not exists `ecodb`.`comaplaints` ( `Compalint_No` INT(5) NOT NULL , `Bank_ID` VARCHAR(10) NOT NULL , `Complaint_IssueDate` DATE NOT NULL , PRIMARY KEY (`Compalint_No`));")
         self.appCursor.execute("CREATE TABLE if not exists `ecodb`.`complaints` ( `invoice_id` VARCHAR(15) NOT NULL ,  `date` DATE NOT NULL ,  `branch_address` VARCHAR(50) NOT NULL ,  `zone` VARCHAR(10) NOT NULL ,  `complaint_no` INT(10) NOT NULL ,    PRIMARY KEY  (`invoice_id`));")
@@ -57,14 +40,9 @@ class Database:
     #     self.dbCursor.execute("select * from ins.students")
     #     for row in self.dbCursor:
     #         print(row[0])
->>>>>>> a505de5579bccfd7715ad9f855441a8de58b635f
 
 
 if __name__ == "__main__":
     db = Database()
-<<<<<<< HEAD
-    db.getdata()
-=======
     print(db.getServiceRate('Monthly general Servicing (upto to 2 Ton)'))
     # db.getdata()
->>>>>>> a505de5579bccfd7715ad9f855441a8de58b635f
